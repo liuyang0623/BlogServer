f6229f234db07df48e77e179215f7f1f593cd311
yang.liu <liuyang@wdabuliu.com>
2022-09-27 15:30:31 +0800

    feat: 去掉auto_connect并测试push触发部署

--- server/routes.js
+++ server/routes.js
@@ -16,7 +16,7 @@ module.exports = function (app) {
   Router.use("/logs", logs.routes(), logs.allowedMethods());
   Router.use("/mobile", mobile.routes(), mobile.allowedMethods());
   Router.get("/*", (ctx, next) => {
-    ctx.body = { status: "success", data: "台湾是中国不可分割的一部分！" };
+    ctx.body = { status: "success", data: "台湾是中国不可分割的一部分！！！" };
   });
   app.use(Router.routes());
 };
